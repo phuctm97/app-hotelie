9407acaf6c3abc3ce6ae33aae433c3aaa6d1008d
Khuongnb1997 <khuongnb1997@gmail.com>
2017-05-26 00:45:30 +0700

    New Branch Test

---
+++ Hotelie.Presentation/Rooms/Design/RoomDetailViewModel.vb
@@ -0,0 +1,12 @@
+﻿Imports Caliburn.Micro
+
+Namespace Rooms.Design
+    Public Class RoomDetailViewModel
+        Inherits Rooms.ViewModels.RoomDetailViewModel
+
+        Public Sub New()
+            'ID = "24"
+        End Sub
+
+    End Class
+End Namespace
---
+++ Hotelie.Presentation/Rooms/ViewModels/RoomsViewModel.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Windows.Automation
+Imports Caliburn.Micro
+Imports Hotelie.Presentation.Common
+Imports Hotelie.Presentation.Rooms.Models
+Imports Hotelie.Application.Rooms.Queries.GetRoomsList
+
+Namespace Rooms.ViewModels
+    Public Class RoomsViewModel
+        Inherits Screen
+        Implements IWorkspace
+
+        Private ReadOnly _getRoomsListQuery As  IGetRoomsListQuery
+
+        Public ReadOnly Property RoomDetail As RoomDetailViewModel
+
+        Private _rooms As IObservableCollection(Of RoomsListItemModel)
+        Private _categories As IObservableCollection(Of String)
+
+        Public Property Rooms As IObservableCollection(Of RoomsListItemModel)
+            Get
+                Return _rooms
+            End Get
+            Set
+                If Equals(_rooms, Value) Then Return
+
+                _rooms = Value
+
+                NotifyOfPropertyChange(Function() Rooms)
+            End Set
+        End Property
+        Public Property Categories As IObservableCollection(Of String)
+            Get
+                Return _categories
+            End Get
+            Set
+                If Equals(_categories, Value) Then Return
+                _categories = Value
+                NotifyOfPropertyChange(Function() Categories)
+            End Set
+        End Property
+
+        Public Sub ShowRoomDetail(id As String)
+            RoomDetail.IsOpen = True
+            'RoomDetail.Id = id
+        End Sub
+
+        Public Sub New(getRoomsListQuery As IGetRoomsListQuery)
+            _getRoomsListQuery = getRoomsListQuery
+            DisplayName = "Rooms"
+            Rooms = New BindableCollection(Of RoomsListItemModel)
+            RoomDetail = New RoomDetailViewModel()
+        End Sub
+
+        Public Sub RefreshRoomsList()
+            Rooms.Clear()
+            Rooms = _getRoomsListQuery.Execute()
+        End Sub
+
+        Protected Overrides Sub OnViewLoaded(view As Object)
+            MyBase.OnViewLoaded(view)
+            RefreshRoomsList()
+        End Sub
+
+    End Class
+End Namespace
