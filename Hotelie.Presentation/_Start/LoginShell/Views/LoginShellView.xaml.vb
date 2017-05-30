f1d44b59fce9608c9443b3648f21d872fda30519
phuctm97 <phuctm97@gmail.com>
2017-05-30 17:39:52 +0700

    Complete login view without transition

--- Hotelie.Presentation/_Start/LoginShell/Views/LoginShellView.xaml.vb
+++ Hotelie.Presentation/_Start/LoginShell/Views/LoginShellView.xaml.vb
@@ -5,6 +5,8 @@ Namespace Start.LoginShell.Views
 	Public Class LoginShellView
 		Private _previousPasswordLength As Integer
 		Private _previousAccountLength As Integer
+		Private _previousServerNameLength As Integer
+		Private _previousDatabaseNameLength As Integer
 
 		Public Sub New()
 
@@ -27,6 +29,8 @@ Namespace Start.LoginShell.Views
 		Private Sub InitEffectParameters()
 			_previousPasswordLength = 0
 			_previousAccountLength = 0
+			_previousServerNameLength = 0
+			_previousDatabaseNameLength = 0
 		End Sub
 
 		' Drag window
@@ -40,11 +44,14 @@ Namespace Start.LoginShell.Views
 
 		Private Sub OnTitleBarMouseDoubleClick( sender As Object,
 		                                        e As MouseButtonEventArgs )
-			If e.ChangedButton = MouseButton.Left And
-			   e.GetPosition( TitleBar ).Y < TitleBar.ActualHeight And
-			   e.GetPosition( WindowCommandPopups ).X < 0
-				OnZoomButtonClick( Nothing, Nothing )
-			End If
+			If Not e.ChangedButton = MouseButton.Left Then Return
+			If e.GetPosition( TitleBar ).Y > TitleBar.ActualHeight Then Return
+			If ButtonSettings.Visibility = Visibility.Visible And
+			   e.GetPosition( ButtonSettings ).X > 0 Then Return
+			If ButtonLogin.Visibility = Visibility.Visible And
+			   e.GetPosition( ButtonLogin ).X > 0 Then Return
+
+			OnZoomButtonClick( Nothing, Nothing )
 		End Sub
 
 		Private Sub OnZoomButtonClick( sender As Object,
@@ -107,6 +114,70 @@ Namespace Start.LoginShell.Views
 			End If
 
 			_previousAccountLength = accountBox.Text.Length
+		End Sub
+
+		Private Sub OnServerChanged( sender As Object,
+		                             e As TextChangedEventArgs )
+			Dim serverBox = CType(sender, TextBox)
+
+			If _previousServerNameLength = 0 And serverBox.Text.Length > 0
+				' animate box down
+				Dim marginAnimation = New ThicknessAnimation( New Thickness( 40, 16, 40, 16 ),
+				                                              New Duration( TimeSpan.FromMilliseconds( 400 ) ) )
+				serverBox.BeginAnimation( MarginProperty, marginAnimation )
+
+			ElseIf _previousServerNameLength > 0 And serverBox.Text.Length = 0
+				' animate box up
+				Dim marginAnimation = New ThicknessAnimation( New Thickness( 40, 0, 40, 16 ),
+				                                              New Duration( TimeSpan.FromMilliseconds( 400 ) ) )
+				serverBox.BeginAnimation( MarginProperty, marginAnimation )
+			End If
+
+			_previousServerNameLength = serverBox.Text.Length
+		End Sub
+
+		Private Sub OnDatabaseChanged( sender As Object,
+		                               e As TextChangedEventArgs )
+			Dim databaseBox = CType(sender, TextBox)
+
+			If _previousDatabaseNameLength = 0 And databaseBox.Text.Length > 0
+				' animate box down
+				Dim marginAnimation = New ThicknessAnimation( New Thickness( 40, 16, 40, 32 ),
+				                                              New Duration( TimeSpan.FromMilliseconds( 400 ) ) )
+				databaseBox.BeginAnimation( MarginProperty, marginAnimation )
+
+			ElseIf _previousDatabaseNameLength > 0 And databaseBox.Text.Length = 0
+				' animate box up
+				Dim marginAnimation = New ThicknessAnimation( New Thickness( 40, 0, 40, 32 ),
+				                                              New Duration( TimeSpan.FromMilliseconds( 400 ) ) )
+				databaseBox.BeginAnimation( MarginProperty, marginAnimation )
+			End If
+
+			_previousDatabaseNameLength = databaseBox.Text.Length
+		End Sub
+
+		Private Sub OnButtonSettingsClick( sender As Object,
+		                                   e As RoutedEventArgs )
+			ButtonSettings.Visibility = Visibility.Collapsed
+			DescriptionSettings.Visibility = Visibility.Collapsed
+
+			ButtonLogin.Visibility = Visibility.Visible
+			DescriptionLogin.Visibility = Visibility.Visible
+
+			FormLogin.Visibility = Visibility.Collapsed
+			FormSettings.Visibility = Visibility.Visible
+		End Sub
+
+		Private Sub OnButtonLoginClick( sender As Object,
+		                                e As RoutedEventArgs )
+			ButtonLogin.Visibility = Visibility.Collapsed
+			DescriptionLogin.Visibility = Visibility.Collapsed
+
+			ButtonSettings.Visibility = Visibility.Visible
+			DescriptionSettings.Visibility = Visibility.Visible
+
+			FormLogin.Visibility = Visibility.Visible
+			FormSettings.Visibility = Visibility.Collapsed
 		End Sub
 	End Class
 End Namespace
