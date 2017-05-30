fcbe0ccc31b2676af47a486b8381681dfbe01b56
phuctm97 <phuctm97@gmail.com>
2017-05-31 01:55:58 +0700

    Add room detail view

--- Hotelie.Presentation/Rooms/Views/RoomDetailView.xaml.vb
+++ Hotelie.Presentation/Rooms/Views/RoomDetailView.xaml.vb
@@ -1,4 +1,20 @@
-﻿Namespace Rooms.Views
+﻿
+
+Namespace Rooms.Views
 	Public Class RoomDetailView
+		Private _previousRoomNameLength As Integer
+
+		Public Sub New()
+
+			' This call is required by the designer.
+			InitializeComponent()
+
+			' Add any initialization after the InitializeComponent() call.
+			InitEffectParameters()
+		End Sub
+
+		Private Sub InitEffectParameters()
+			_previousRoomNameLength = 0
+		End Sub
 	End Class
 End Namespace
