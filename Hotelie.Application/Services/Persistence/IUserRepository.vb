5775172e12483b45884ee6e12a105b09a8413f44
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-01 23:24:50 +0700

    IUserRepository Update UserCategory

--- Hotelie.Application/Services/Persistence/IUserRepository.vb
+++ Hotelie.Application/Services/Persistence/IUserRepository.vb
@@ -1,8 +1,23 @@
-﻿Imports Hotelie.Domain.Users
+﻿Imports System.Linq.Expressions
+Imports Hotelie.Domain.Users
 
 Namespace Services.Persistence
     Public Interface IUserRepository
         Inherits IRepository(Of User)
+
+        Function GetRoomCategory( id As Object ) As UserCategory
+
+        Function GetAllRoomCategories() As IQueryable(Of UserCategory)
+
+        Function FindRoomCategory( predicate As Expression(Of Func(Of UserCategory, Boolean)) ) As IQueryable(Of UserCategory)
+
+        Sub AddRoomCategory( entity As UserCategory )
+
+        Sub AddRoomCategories( entities As IEnumerable(Of UserCategory) )
+
+        Sub RemoveRoomCategory( entity As UserCategory )
+
+        Sub RemoveRoomCategories( entities As IEnumerable(Of UserCategory) )
 
     End Interface
 End NameSpace
