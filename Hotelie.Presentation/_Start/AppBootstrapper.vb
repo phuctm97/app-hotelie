c8239fac3e51954bc869ded209163852eef01f01
phuctm97 <phuctm97@gmail.com>
2017-06-01 14:22:17 +0700

    Add registry

--- Hotelie.Presentation/_Start/AppBootstrapper.vb
+++ Hotelie.Presentation/_Start/AppBootstrapper.vb
@@ -1,6 +1,4 @@
-﻿Imports Caliburn.Micro
-Imports Hotelie.Application.Services.Authentication
-Imports Hotelie.Presentation.Common
+﻿Imports Hotelie.Presentation.Common
 Imports Hotelie.Presentation.Leases.ViewModels
 Imports Hotelie.Presentation.Rooms.ViewModels
 Imports Hotelie.Presentation.Start.LoginShell.ViewModels
@@ -10,21 +8,10 @@ Imports Microsoft.Practices.Unity
 
 Namespace Start
 	Public Class AppBootstrapper
-		Inherits BootstrapperBase
+		Inherits Registry.AppBootstrapper
 
-		Private _container As IUnityContainer
-
-		Public Sub New()
-			Initialize()
-		End Sub
-
-		Protected Overrides Sub Configure()
-			MyBase.Configure()
-
-			' Dependencies resolution
-			_container = New UnityContainer()
-			_container.RegisterType(Of IWindowManager, WindowManager)( New ContainerControlledLifetimeManager() )
-			_container.RegisterType(Of IEventAggregator, EventAggregator)( New ContainerControlledLifetimeManager() )
+		Protected Overrides Sub ComposeDependencies()
+			MyBase.ComposeDependencies()
 
 			' Start
 			_container.RegisterType(Of IMainWindow, MainWindowViewModel)( New ContainerControlledLifetimeManager() )
@@ -35,31 +22,6 @@ Namespace Start
 			_container.RegisterType(Of IWorkspace, RoomsWorkspaceViewModel)( "rooms-workspace", New TransientLifetimeManager() )
 			_container.RegisterType(Of IWorkspace, LeasesWorkspaceViewModel)( "leases-workspace",
 			                                                                  New TransientLifetimeManager() )
-
-			' Authentication
-			_container.RegisterType(Of IAuthentication, Tests.Authentication)( New ContainerControlledLifetimeManager() )
-
-			'_container.RegisterType(Of IGetRoomsListQuery, ExampleRoomsListItem)( new ContainerControlledLifetimeManager() )
-		End Sub
-
-		Protected Overrides Function GetInstance( service As Type,
-		                                          key As String ) As Object
-			Return _container.Resolve( service, key )
-		End Function
-
-		Protected Overrides Function GetAllInstances( service As Type ) As IEnumerable(Of Object)
-			Return _container.ResolveAll( service )
-		End Function
-
-		Protected Overrides Sub BuildUp( instance As Object )
-			_container.BuildUp( instance )
-		End Sub
-
-		Protected Overrides Sub OnExit( sender As Object,
-		                                e As EventArgs )
-			MyBase.OnExit( sender, e )
-
-			_container.Dispose()
 		End Sub
 
 		Protected Overrides Sub OnStartup( sender As Object,
---
+++ Hotelie.Registry/AppBootstrapper.vb
@@ -0,0 +1,47 @@
+﻿Imports Caliburn.Micro
+Imports Hotelie.Application.Services.Authentication
+Imports Microsoft.Practices.Unity
+
+Public Class AppBootstrapper
+	Inherits BootstrapperBase
+
+	Protected ReadOnly _container As IUnityContainer
+
+	Protected Sub New()
+		_container = New UnityContainer()
+
+		Initialize()
+	End Sub
+
+	Protected Overrides Sub Configure()
+		MyBase.Configure()
+
+		ComposeDependencies()
+	End Sub
+
+	Protected Overridable Sub ComposeDependencies()
+		_container.RegisterType(Of IWindowManager, WindowManager)( New ContainerControlledLifetimeManager() )
+		_container.RegisterType(Of IEventAggregator, EventAggregator)( New ContainerControlledLifetimeManager() )
+		_container.RegisterType(Of IAuthentication, Tests.Authentication)( New ContainerControlledLifetimeManager() )
+	End Sub
+
+	Protected Overrides Function GetInstance( service As Type,
+	                                          key As String ) As Object
+		Return _container.Resolve( service, key )
+	End Function
+
+	Protected Overrides Function GetAllInstances( service As Type ) As IEnumerable(Of Object)
+		Return _container.ResolveAll( service )
+	End Function
+
+	Protected Overrides Sub BuildUp( instance As Object )
+		_container.BuildUp( instance )
+	End Sub
+
+	Protected Overrides Sub OnExit( sender As Object,
+	                                e As EventArgs )
+		MyBase.OnExit( sender, e )
+
+		_container.Dispose()
+	End Sub
+End Class
