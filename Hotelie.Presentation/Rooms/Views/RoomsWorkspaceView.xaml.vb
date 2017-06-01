b730189a8362bfbfba5f89ca2e0cc75860532b79
phuctm97 <phuctm97@gmail.com>
2017-06-02 03:28:46 +0700

    finish bind

--- Hotelie.Presentation/Rooms/Views/RoomsWorkspaceView.xaml.vb
+++ Hotelie.Presentation/Rooms/Views/RoomsWorkspaceView.xaml.vb
@@ -1,4 +1,6 @@
 ﻿
+Imports Hotelie.Application.Rooms.Queries.GetRoomCategoriesList
+Imports Hotelie.Application.Rooms.Queries.GetRoomsList
 
 Namespace Rooms.Views
 	Public Class RoomsWorkspaceView
@@ -7,6 +9,30 @@ Namespace Rooms.Views
 			TextBoxSearchName.Text = String.Empty
 			ComboBoxSearchState.SelectedIndex = ComboBoxSearchState.Items.Count - 1
 			ComboBoxSearchCategory.SelectedIndex = ComboBoxSearchCategory.Items.Count - 1
+		End Sub
+
+		Private Sub OnButtonRoomCategoryDisplayClicked( sender As Object,
+		                                                e As RoutedEventArgs )
+			Dim button = CType(sender, Button)
+			Dim room = CType(button.DataContext, RoomModel)
+
+			For Each category As RoomCategoryModel In ComboBoxSearchCategory.ItemsSource
+				If room.CategoryId = category.Id
+					ComboBoxSearchCategory.SelectedItem = category
+				End If
+			Next
+		End Sub
+
+		Private Sub OnButtonRoomStateDisplayClicked( sender As Object,
+		                                             e As RoutedEventArgs )
+			Dim button = CType(sender, Button)
+			Dim room = CType(button.DataContext, RoomModel)
+
+			For Each state As Integer In ComboBoxSearchState.ItemsSource
+				If room.State = state
+					ComboBoxSearchState.SelectedItem = state
+				End If
+			Next
 		End Sub
 	End Class
 End Namespace
