f09d587e9316517f1f68808f57c46f0e3b6f2a0f
phuctm97 <phuctm97@gmail.com>
2017-06-01 17:15:08 +0700

    update registry test get rooms list

--- Hotelie.Registry/Tests/Rooms/Queries/GetRoomsList/GetRoomsListQuery.vb
+++ Hotelie.Registry/Tests/Rooms/Queries/GetRoomsList/GetRoomsListQuery.vb
@@ -8,14 +8,14 @@ Namespace Tests.Rooms.Queries.GetRoomsList
 
 		Public Sub New()
 			_tempRooms = New List(Of RoomModel) From {
-				New RoomModel With {.Name="A101", .CategoryName="Phòng thường 1", .Price=100000, .State=1},
-				New RoomModel With {.Name="A102", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
-				New RoomModel With {.Name="A103", .CategoryName="Phòng Vip 1", .Price=300000, .State=0},
-				New RoomModel With {.Name="A104", .CategoryName="Phòng Vip 2", .Price=450000, .State=1},
-				New RoomModel With {.Name="A201", .CategoryName="Phòng thường 1", .Price=100000, .State=0},
-				New RoomModel With {.Name="A202", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
-				New RoomModel With {.Name="A203", .CategoryName="Phòng Vip 1", .Price=300000, .State=1},
-				New RoomModel With {.Name="A204", .CategoryName="Phòng Vip 2", .Price=450000, .State=0}}
+				New RoomModel With {.Id="1", .Name="A101", .CategoryId="1", .CategoryName="Phòng thường 1", .Price=100000, .State=1},
+				New RoomModel With {.Id="2", .Name="A102", .CategoryId="2", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
+				New RoomModel With {.Id="3", .Name="A103", .CategoryId="3", .CategoryName="Phòng Vip 1", .Price=300000, .State=0},
+				New RoomModel With {.Id="4", .Name="A104", .CategoryId="4", .CategoryName="Phòng Vip 2", .Price=450000, .State=1},
+				New RoomModel With {.Id="5", .Name="A201", .CategoryId="5", .CategoryName="Phòng thường 1", .Price=100000, .State=0},
+				New RoomModel With {.Id="6", .Name="A202", .CategoryId="6", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
+				New RoomModel With {.Id="7", .Name="A203", .CategoryId="7", .CategoryName="Phòng Vip 1", .Price=300000, .State=1},
+				New RoomModel With {.Id="8", .Name="A204", .CategoryId="8", .CategoryName="Phòng Vip 2", .Price=450000, .State=0}}
 		End Sub
 
 		Public Function Execute() As IEnumerable(Of RoomModel) Implements IGetRoomsListQuery.Execute
