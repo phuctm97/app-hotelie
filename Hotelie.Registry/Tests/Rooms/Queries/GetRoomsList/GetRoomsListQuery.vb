9c1546e7b95910e3f1447290aeb200fe801fa69a
phuctm97 <phuctm97@gmail.com>
2017-06-01 16:49:52 +0700

    update test get rooms list query

--- Hotelie.Registry/Tests/Rooms/Queries/GetRoomsList/GetRoomsListQuery.vb
+++ Hotelie.Registry/Tests/Rooms/Queries/GetRoomsList/GetRoomsListQuery.vb
@@ -8,8 +8,14 @@ Namespace Tests.Rooms.Queries.GetRoomsList
 
 		Public Sub New()
 			_tempRooms = New List(Of RoomModel) From {
-				New RoomModel With {.Name="A101", .CategoryName="Phòng thường", .Price=100000, .State=0},
-				New RoomModel With {.Name="A102", .CategoryName="Phòng thường", .Price=150000, .State=0}}
+				New RoomModel With {.Name="A101", .CategoryName="Phòng thường 1", .Price=100000, .State=1},
+				New RoomModel With {.Name="A102", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
+				New RoomModel With {.Name="A103", .CategoryName="Phòng Vip 1", .Price=300000, .State=0},
+				New RoomModel With {.Name="A104", .CategoryName="Phòng Vip 2", .Price=450000, .State=1},
+				New RoomModel With {.Name="A201", .CategoryName="Phòng thường 1", .Price=100000, .State=0},
+				New RoomModel With {.Name="A202", .CategoryName="Phòng thường 2", .Price=150000, .State=0},
+				New RoomModel With {.Name="A203", .CategoryName="Phòng Vip 1", .Price=300000, .State=1},
+				New RoomModel With {.Name="A204", .CategoryName="Phòng Vip 2", .Price=450000, .State=0}}
 		End Sub
 
 		Public Function Execute() As IEnumerable(Of RoomModel) Implements IGetRoomsListQuery.Execute
