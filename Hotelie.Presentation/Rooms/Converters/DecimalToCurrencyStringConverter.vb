14df54058ea676d37e499e545a34deb27dd469ef
phuctm97 <phuctm97@gmail.com>
2017-06-01 17:03:42 +0700

    Converter price

---
+++ Hotelie.Presentation/Rooms/Converters/DecimalToCurrencyStringConverter.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Globalization
+
+Namespace Rooms.Converters
+	Public Class DecimalToCurrencyStringConverter
+		Implements IValueConverter
+
+		Public Function Convert( value As Object,
+		                         targetType As Type,
+		                         parameter As Object,
+		                         culture As CultureInfo ) As Object Implements IValueConverter.Convert
+			Dim x = CType(value, Decimal)
+
+			Return x.ToString( "N0" )
+		End Function
+
+		Public Function ConvertBack( value As Object,
+		                             targetType As Type,
+		                             parameter As Object,
+		                             culture As CultureInfo ) As Object Implements IValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
+	End Class
+End Namespace
