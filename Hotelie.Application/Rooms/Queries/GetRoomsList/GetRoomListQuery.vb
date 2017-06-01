c0b43706af3c7286659b7845d1f1ae296146cd3b
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-02 03:28:50 +0700

    GetRoomsList RoomCategoriesList Tested

--- Hotelie.Application/Rooms/Queries/GetRoomsList/GetRoomListQuery.vb
+++ Hotelie.Application/Rooms/Queries/GetRoomsList/GetRoomListQuery.vb
@@ -1,4 +1,5 @@
-﻿Imports Hotelie.Application.Services.Persistence
+﻿Imports System.Windows.Media
+Imports Hotelie.Application.Services.Persistence
 
 Namespace Rooms.Queries.GetRoomsList
     Public Class GetRoomsListQuery
@@ -6,20 +7,27 @@ Namespace Rooms.Queries.GetRoomsList
 
         Private ReadOnly _roomRepository As IRoomRepository
 
-        Sub New( roomRepository As IRoomRepository )
+        Sub New(roomRepository As IRoomRepository)
             _roomRepository = roomRepository
         End Sub
 
         Public Function Execute() As IEnumerable(Of RoomModel) Implements IGetRoomsListQuery.Execute
             Dim rooms =
                     _roomRepository.GetAll().Select(
-                        Function( r ) New RoomModel With _
-                                                       { .Name=r.Name,
+                        Function(r) New RoomModel With _
+                                                       { .Id = r.Id,
+                                                       .State = r.State,
+                                                       .Note = r.Note,
+                                                       .Name = r.Name,
+                                                       .CategoryId=r.Category.Id,
                                                        .CategoryName=r.Category.Name,
-                                                       .Note=r.Note,
-                                                       .Price=r.Category.Price,
-                                                       .State=r.State} )
-
+                                                       .Price = r.Category.Price
+                                                       })
+            ' Exception: System.NotSupportedException Unable to create a constant value of type 'System.Windows.Media.Color'
+            ' TODO: Add Category display color column to database -> add ".CategoryDisplayColor = r.CategoryDisplayColor"
+            For Each room As RoomModel In rooms
+                room.CategoryDisplayColor = Colors.Black
+            Next
             Return rooms
         End Function
     End Class
---
+++ Tests/Hotelie.Application.Test/Query/TestGetRoomCategoriesListQuery.vb
@@ -0,0 +1,74 @@
+﻿Imports Hotelie.Application.Rooms.Queries.GetRoomCategoriesList
+Imports Hotelie.Application.Rooms.Queries.GetRoomsList
+Imports Hotelie.Domain.Rooms
+Imports Hotelie.Persistence.Common
+Imports Hotelie.Persistence.Rooms
+
+Namespace Query
+    <TestClass>
+    Public Class TestGetRoomCategoriesListQuery
+        Private _context As DatabaseContext
+        Private _roomRepository As RoomRepository
+        Private _roomCategoriesList As List(Of RoomCategory)
+        Private _getRoomCategoriesListQuery As GetRoomCategoriesListQuery
+
+        <TestInitialize>
+        Public Sub TestInitialize()
+            _context = New DatabaseContext(
+                $"data source=KHUONG-ASUS\SQLEXPRESS;initial catalog=HotelieDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework")
+            _roomRepository = New RoomRepository(_context)
+            _getRoomCategoriesListQuery = New GetRoomCategoriesListQuery(_roomRepository)
+        End Sub
+
+        <TestCleanup>
+        Public Sub TestCleanup()
+            _context.Dispose()
+        End Sub
+
+        Public Sub RoomCategoriesInitialize()
+            _roomCategoriesList = New List(Of RoomCategory)
+            Dim roomCategory1 = New RoomCategory() With{.Id="00001",.Name="NOR01",.Price=100000}
+            Dim roomCategory2 = New RoomCategory() With{.Id="00002",.Name="VIP01",.Price=200000}
+            _roomCategoriesList.Add(roomCategory1)
+            _roomCategoriesList.Add(roomCategory2)
+            _roomRepository.AddRoomCategories(_roomCategoriesList)
+            _context.SaveChanges()
+        End Sub
+
+        Public Sub DisposeRoomCategories()
+            _roomCategoriesList?.Clear()
+            _roomRepository.RemoveRoomCategories(_roomRepository.GetAllRoomCategories())
+            _context.SaveChanges()
+        End Sub
+
+        <TestMethod>
+        Public Sub TestGetRoomCategories__ReturnAllCategory()
+            ' pre-act
+            DisposeRoomCategories()
+
+            ' input
+            RoomCategoriesInitialize()
+
+            ' act
+            Dim roomCategories = _getRoomCategoriesListQuery.Execute().ToList()
+
+            ' assert
+            CollectionAssert.AllItemsAreNotNull(roomCategories)
+            Assert.AreEqual(_roomCategoriesList.Count, roomCategories.Count)
+
+            For Each room As RoomCategory In _roomCategoriesList
+                Dim q = False
+                For Each rc As RoomCategoryModel In roomCategories
+                    If room.Id= rc.Id Then 
+                        q = True
+                        Exit For
+                    End If
+                Next
+                Assert.IsTrue(q)
+            Next
+
+            ' rollback
+            DisposeRoomCategories()
+        End Sub
+    End Class
+End Namespace
