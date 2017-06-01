56b526e611270a0664e4411a19f60705578484ff
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-01 16:05:04 +0700

    Update room repository

--- Hotelie.Application/Services/Persistence/IRoomRepository.vb
+++ Hotelie.Application/Services/Persistence/IRoomRepository.vb
@@ -1,8 +1,23 @@
-﻿Imports Hotelie.Domain.Rooms
+﻿Imports System.Linq.Expressions
+Imports Hotelie.Domain.Rooms
 
 Namespace Services.Persistence
 	Public Interface IRoomRepository
 		Inherits IRepository(Of Room)
+
+	    Function GetRoomCategory( id As Object ) As RoomCategory
+
+	    Function GetAllRoomCategories() As IQueryable(Of RoomCategory)
+
+	    Function FindRoomCategory( predicate As Expression(Of Func(Of RoomCategory, Boolean)) ) As IQueryable(Of RoomCategory)
+
+	    Sub AddRoomCategory( entity As RoomCategory )
+
+	    Sub AddRoomCategories( entities As IEnumerable(Of RoomCategory) )
+
+	    Sub RemoveRoomCategory( entity As RoomCategory )
+
+	    Sub RemoveRoomCategories( entities As IEnumerable(Of RoomCategory) )
 
 	End Interface
 End Namespace
