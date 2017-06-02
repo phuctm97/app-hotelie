c556548a1f3eb93fbfaba13073e83c4255b286b8
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-03 03:55:18 +0700

    Remove + Update lease command added

---
+++ Hotelie.Application/Leases/Queries/GetLeasesListQuery.vb
@@ -0,0 +1,27 @@
+﻿Imports Hotelie.Application.Services.Persistence
+
+Namespace Leases.Queries
+    Public Class GetLeasesListQuery
+        Implements IGetLeasesListQuery
+
+        Private ReadOnly _leaseRepository As ILeaseRepository
+
+        Sub New(leaseRepository As ILeaseRepository)
+            _leaseRepository = leaseRepository
+        End Sub
+
+        Public Function Execute() As IEnumerable(Of LeaseModel) Implements IGetLeasesListQuery.Execute
+            Dim leases = _leaseRepository.GetAll().Select(Function(p) New LeaseModel() With _
+                                                        {.Id = p.Id,
+                                                        .RoomId = p.Room.Id,
+                                                        .BeginDate = p.BeginDate,
+                                                        .EndDate = p.EndDate,
+                                                        .ExtraCoefficient = p.ExtraCoefficient,
+                                                        .CustomerCoefficient = p.CustomerCoefficient,
+                                                        .ExtraCharge = p.ExtraCharge,
+                                                        .NumberOfDate = p.NumberOfDate,
+                                                        .BillId = p.Bill.Id})
+            Return leases
+        End Function
+    End Class
+End NameSpace
