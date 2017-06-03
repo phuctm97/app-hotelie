a291e51445249c75f1036c45e5e1b46cf7783de5
phuctm97 <phuctm97@gmail.com>
2017-06-03 23:47:12 +0700

    d

---
+++ Hotelie.Registry/Tests/Services/Persistence/DatabaseService.vb
@@ -0,0 +1,26 @@
+﻿Imports Hotelie.Application.Services.Persistence
+
+Namespace Tests.Services.Persistence
+	Public Class DatabaseService
+		Implements IDatabaseService
+
+		Public ReadOnly Property Context As IDatabaseContext Implements IDatabaseService.Context
+			Get
+				Throw New NotImplementedException()
+			End Get
+		End Property
+
+		Public Sub Dispose() Implements IDatabaseService.Dispose
+			Throw New NotImplementedException()
+		End Sub
+
+		Public Sub SetDatabaseConnection(connectionString As String) Implements IDatabaseService.SetDatabaseConnection
+			Throw New NotImplementedException()
+		End Sub
+
+		Public Function CheckDatabaseConnection(connectionString As String) As Boolean Implements IDatabaseService.CheckDatabaseConnection
+			Throw New NotImplementedException()
+		End Function
+
+	End Class
+End Namespace
