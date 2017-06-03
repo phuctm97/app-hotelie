4ca16c77dd4b33c4e4c60a15de9d0114747f1812
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-03 17:25:13 +0700

    DatabaseService Initialize

---
+++ Hotelie.Persistence/DatabaseServices/DatabaseService.vb
@@ -0,0 +1,14 @@
+﻿Namespace DatabaseServices
+    Public Class DatabaseService
+        Implements IDatabaseService
+
+        Private _context As DatabaseContext
+
+        Public Sub SetDatabaseConnection(connectionString As String) Implements IDatabaseService.SetDatabaseConnection
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Function CheckDatabaseConnection(connectionString As String) As Boolean Implements IDatabaseService.CheckDatabaseConnection
+        End Function
+    End Class
+End NameSpace
