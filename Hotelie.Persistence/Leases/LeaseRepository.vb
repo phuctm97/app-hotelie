53183f2c805e0f4cb2341e7f2f3ecd3d1b2119df
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-04 06:55:15 +0700

    Async GetOne Adding

--- Hotelie.Persistence/Leases/LeaseRepository.vb
+++ Hotelie.Persistence/Leases/LeaseRepository.vb
@@ -1,4 +1,5 @@
-﻿Imports Hotelie.Application.Services.Persistence
+﻿Imports System.Data.Entity
+Imports Hotelie.Application.Services.Persistence
 Imports Hotelie.Domain.Leases
 Imports Hotelie.Persistence.Common
 
@@ -18,11 +19,23 @@ Namespace Leases
             Return _databaseService.Context.LeaseDetails.Where(Function(p)p.Lease.Id = id).ToList()
         End Function
 
+        Public Async Function GetCustomersAsync(id As String) As Task(Of List(Of LeaseDetail)) Implements ILeaseRepository.GetCustomersAsync
+            Dim curs = Await _databaseService.Context.LeaseDetails.Where(Function(p)p.Lease.Id = id).ToListAsync()
+            Return curs
+        End Function
+
         Public Overrides Function GetOne(id As Object) As Lease
             Dim idString = CType(id, String)
             If idString Is Nothing Then Throw New InvalidCastException("Id must be string")
 
             Return _databaseService.Context.Leases.FirstOrDefault(Function(p) String.Equals(p.Id, idString))
+        End Function
+
+        Public Overrides Async Function GetOneAsync(id As Object) As Task(Of Lease)
+            Dim idString = CType(id, String)
+            If idString Is Nothing Then Throw New InvalidCastException("Id must be string")
+            Dim lease = Await _databaseService.Context.Leases.FirstOrDefaultAsync(Function(p) String.Equals(p.Id, idString))
+            Return lease
         End Function
     End Class
 End Namespace
