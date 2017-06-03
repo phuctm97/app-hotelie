0a317603e73c8f23cb9b2ab3237a87718ff677d2
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-04 02:28:29 +0700

    Registry Adding

--- Hotelie.Registry/AppBootstrapper.vb
+++ Hotelie.Registry/AppBootstrapper.vb
@@ -1,55 +1,62 @@
 ﻿Imports Caliburn.Micro
+Imports Hotelie.Application.Leases.Queries
+Imports Hotelie.Application.Rooms.Queries.GetRoomCategoriesList
+Imports Hotelie.Application.Rooms.Queries.GetRoomsList
 Imports Hotelie.Application.Services.Authentication
 Imports Hotelie.Application.Services.Persistence
 Imports Hotelie.Persistence.Common
+Imports Hotelie.Persistence.Leases
 Imports Hotelie.Persistence.Users
 Imports Microsoft.Practices.Unity
 
 Public Class AppBootstrapper
-	Inherits BootstrapperBase
+    Inherits BootstrapperBase
 
-	Protected ReadOnly _container As IUnityContainer
+    Protected ReadOnly _container As IUnityContainer
 
-	Protected Sub New()
-		_container = New UnityContainer()
+    Protected Sub New()
+        _container = New UnityContainer()
 
-		Initialize()
-	End Sub
+        Initialize()
+    End Sub
 
-	Protected Overrides Sub Configure()
-		MyBase.Configure()
+    Protected Overrides Sub Configure()
+        MyBase.Configure()
 
-		ComposeDependencies()
-	End Sub
+        ComposeDependencies()
+    End Sub
 
-	Protected Overridable Sub ComposeDependencies()
-		_container.RegisterType(Of IWindowManager, WindowManager)( New ContainerControlledLifetimeManager() )
-		_container.RegisterType(Of IEventAggregator, EventAggregator)( New ContainerControlledLifetimeManager() )
+    Protected Overridable Sub ComposeDependencies()
+        _container.RegisterType(Of IWindowManager, WindowManager)( New ContainerControlledLifetimeManager() )
+        _container.RegisterType(Of IEventAggregator, EventAggregator)( New ContainerControlledLifetimeManager() )
         _container.RegisterType(Of IUserRepository, UserRepository)(New ContainerControlledLifetimeManager())
-		_container.RegisterType(Of IAuthentication, Authentication)( New ContainerControlledLifetimeManager() )
-	    _container.RegisterType(Of IDatabaseService, DatabaseService)(New ContainerControlledLifetimeManager())
+        _container.RegisterType(Of IAuthentication, Authentication)( New ContainerControlledLifetimeManager() )
+        _container.RegisterType(Of IDatabaseService, DatabaseService)(New ContainerControlledLifetimeManager())
         _container.RegisterType(Of IGetLeasesListQuery, GetLeasesListQuery)(New ContainerControlledLifetimeManager())
-	    _container.RegisterType(Of IRoomRepository, IRoomRepository)(New ContainerControlledLifetimeManager())
-	    _container.RegisterType(Of IGetRoomsListQuery, GetRoomListQuery)(New ContainerControlledLifetimeManager())
-	End Sub
+        _container.RegisterType(Of IRoomRepository, IRoomRepository)(New ContainerControlledLifetimeManager())
+        _container.RegisterType(Of ILeaseRepository, LeaseRepository)(New ContainerControlledLifetimeManager())
+        _container.RegisterType(Of IGetRoomsListQuery, GetRoomsListQuery)(New ContainerControlledLifetimeManager())
+        _container.RegisterType _
+            (Of IGetRoomCategoriesListQuery, GetRoomCategoriesListQuery)(New ContainerControlledLifetimeManager())
+    End Sub
 
-	Protected Overrides Function GetInstance( service As Type,
-	                                          key As String ) As Object
-		Return _container.Resolve( service, key )
-	End Function
+    Protected Overrides Function GetInstance( service As Type,
+                                              key As String ) As Object
+        Return _container.Resolve( service, key )
+    End Function
 
-	Protected Overrides Function GetAllInstances( service As Type ) As IEnumerable(Of Object)
-		Return _container.ResolveAll( service )
-	End Function
+    Protected Overrides Function GetAllInstances( service As Type ) As IEnumerable(Of Object)
+        Return _container.ResolveAll( service )
+    End Function
 
-	Protected Overrides Sub BuildUp( instance As Object )
-		_container.BuildUp( instance )
-	End Sub
+    Protected Overrides Sub BuildUp( instance As Object )
+        _container.BuildUp( instance )
+    End Sub
 
-	Protected Overrides Sub OnExit( sender As Object,
-	                                e As EventArgs )
-		MyBase.OnExit( sender, e )
+    Protected Overrides Sub OnExit( sender As Object,
+                                    e As EventArgs )
+        MyBase.OnExit( sender, e )
 
-		_container.Dispose()
-	End Sub
+        _container.Dispose()
+    End Sub
 End Class
