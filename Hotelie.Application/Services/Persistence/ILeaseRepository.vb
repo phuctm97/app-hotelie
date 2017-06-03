ce56bc4a638636fa6891ce8fd237e1db4adb1101
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-03 15:54:04 +0700

    Fixing lease's customers 2

--- Hotelie.Application/Services/Persistence/ILeaseRepository.vb
+++ Hotelie.Application/Services/Persistence/ILeaseRepository.vb
@@ -3,6 +3,6 @@
 Namespace Services.Persistence
     Public Interface ILeaseRepository
         Inherits IRepository(Of Lease)
-
+        Function GetCustomers() As List(Of LeaseDetail)
     End Interface
 End NameSpace
