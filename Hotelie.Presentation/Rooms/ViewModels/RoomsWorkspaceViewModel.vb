8f403718ad63c0b43e4f54c6ffa9a12fbc5a4e01
phuctm97 <phuctm97@gmail.com>
2017-06-03 16:55:26 +0700

    Add ScreenRoomDetail

--- Hotelie.Presentation/Rooms/ViewModels/RoomsWorkspaceViewModel.vb
+++ Hotelie.Presentation/Rooms/ViewModels/RoomsWorkspaceViewModel.vb
@@ -6,13 +6,43 @@ Namespace Rooms.ViewModels
 	Public Class RoomsWorkspaceViewModel
 		Inherits Screen
 
+		Private _displayCode As Integer
+
 		Public ReadOnly Property ScreenRoomsList As ScreenRoomsListViewModel
+
+		Public ReadOnly Property ScreenRoomDetail As ScreenRoomDetailViewModel
+
+		Public Property DisplayCode As Integer
+			Get
+				Return _displayCode
+			End Get
+			Set
+				If Equals( Value, _displayCode ) Then Return
+				_displayCode = value
+				NotifyOfPropertyChange( Function() DisplayCode )
+			End Set
+		End Property
 
 		Public Sub New( getRoomsListQuery As IGetRoomsListQuery,
 		                getRoomCategoriesListQuery As IGetRoomCategoriesListQuery )
 			DisplayName = "Danh sách phòng"
 
 			ScreenRoomsList = New ScreenRoomsListViewModel( getRoomsListQuery, getRoomCategoriesListQuery )
+
+			ScreenRoomDetail = New ScreenRoomDetailViewModel( getRoomCategoriesListQuery )
+
+			DisplayCode = 0
+		End Sub
+
+		Public Sub NavigateToScreenRoomsList()
+			DisplayCode = 0
+		End Sub
+
+		Public Sub NavigateToScreenRoomDetail( room As RoomModel )
+			If IsNothing(room) Then Return
+
+			ScreenRoomDetail.SetRoom( room.Id, room.Name, room.CategoryId, room.State, room.Note )
+			DisplayCode = 1
 		End Sub
 	End Class
 End Namespace
---
+++ Hotelie.Presentation/Rooms/ViewModels/ScreenRoomDetailViewModel.vb
@@ -0,0 +1,84 @@
+﻿Imports Caliburn.Micro
+Imports Hotelie.Application.Rooms.Queries.GetRoomCategoriesList
+
+Namespace Rooms.ViewModels
+	Public Class ScreenRoomDetailViewModel
+		Inherits PropertyChangedBase
+
+		' Dependencies
+		Private ReadOnly _getRoomCategoriesList As IGetRoomCategoriesListQuery
+
+		Private _roomName As String
+		Private _roomCategory As RoomCategoryModel
+		Private _roomNote As String
+		Private _roomState As Integer
+
+		Sub New( getRoomCategoriesList As IGetRoomCategoriesListQuery )
+			_getRoomCategoriesList = getRoomCategoriesList
+
+			RoomCategories = New BindableCollection(Of RoomCategoryModel)
+			RoomCategories.AddRange( _getRoomCategoriesList.Execute() )
+		End Sub
+
+		Private Property RoomId As String
+
+		Public Property RoomName As String
+			Get
+				Return _roomName
+			End Get
+			Set
+				If String.Equals( Value, _roomName ) Then Return
+				_roomName = value
+				NotifyOfPropertyChange( Function() RoomName )
+			End Set
+		End Property
+
+		Public Property RoomCategory As RoomCategoryModel
+			Get
+				Return _roomCategory
+			End Get
+			Set
+				If Equals(Value, _roomCategory) Then Return
+				_roomCategory = value
+				NotifyOfPropertyChange(Function() RoomCategory)
+			End Set
+		End Property
+
+		Public Property RoomNote As String
+			Get
+				Return _roomNote
+			End Get
+			Set
+				If String.Equals(Value, _roomNote) Then Return
+				_roomNote = value
+				NotifyOfPropertyChange(Function() RoomNote)
+			End Set
+		End Property
+
+		Public Property RoomState As Integer
+			Get
+				Return _roomState
+			End Get
+			Set
+				If Equals(Value, _roomState) Then Return
+				_roomState = value
+				NotifyOfPropertyChange(Function() RoomState)
+			End Set
+		End Property
+
+		' ReSharper disable once CollectionNeverUpdated.Global
+		Public ReadOnly Property RoomCategories As IObservableCollection(Of RoomCategoryModel)
+
+		Public Sub SetRoom( id As String,
+		                    name As String,
+		                    categoryId As String,
+		                    state As Integer,
+		                    note As String )
+			RoomId = id
+			RoomName = name
+			RoomCategory = RoomCategories.FirstOrDefault( Function( c ) Equals( c.Id, categoryId ) )
+			RoomState = state
+			RoomNote = note
+		End Sub
+	End Class
+End Namespace
