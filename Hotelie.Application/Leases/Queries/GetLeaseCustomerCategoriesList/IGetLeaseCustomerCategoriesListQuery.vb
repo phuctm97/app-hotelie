cc081ea598570e3b3140a19b0d4c79425f9f7384
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-06 17:18:10 +0700

    New application

--- Hotelie.Application/Leases/Queries/GetLeaseCustomerCategoriesList/IGetLeaseCustomerCategoriesListQuery.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace Leases.Queries.GetLeaseCustomerCategoriesList
-	Public Interface IGetLeaseCustomerCategoriesListQuery
-		Function Execute() As IEnumerable(Of LeaseCustomerCategoryListItemModel)
-
-		Function ExecuteAsync() As Task(Of IEnumerable(Of LeaseCustomerCategoryListItemModel))
-	End Interface
-End Namespace
