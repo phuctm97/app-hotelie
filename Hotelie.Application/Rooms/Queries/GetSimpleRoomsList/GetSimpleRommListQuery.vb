5626b2de003c0b1bf774c43d8b6bbeedd8bc2f46
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-06 17:26:52 +0700

    Reup merge

---
+++ Hotelie.Application/Rooms/Queries/GetSimpleRoomsList/GetSimpleRommListQuery.vb
@@ -0,0 +1,21 @@
+﻿Imports Hotelie.Application.Services.Persistence
+
+Namespace Rooms.Queries.GetSimpleRoomsList
+    Public Class GetSimpleRommListQuery
+        Implements IGetSimpleRoomsListQuery
+        
+        Private _roomRepository As IRoomRepository
+
+        Sub New(roomRepository As IRoomRepository)
+            _roomRepository = roomRepository
+        End Sub
+
+        Public Function Execute() As IEnumerable(Of SimpleRoomsListItemModel) Implements IGetSimpleRoomsListQuery.Execute
+            Return Nothing
+        End Function
+
+        Public Function ExecuteAsync() As Task(Of IEnumerable(Of SimpleRoomsListItemModel)) Implements IGetSimpleRoomsListQuery.ExecuteAsync
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End NameSpace
