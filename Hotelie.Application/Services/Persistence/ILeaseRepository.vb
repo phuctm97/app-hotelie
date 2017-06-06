ca75595ad0ea138aa6dd3a18186045c81c3d2d4d
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-06 19:42:01 +0700

    Discard get details

--- Hotelie.Application/Services/Persistence/ILeaseRepository.vb
+++ Hotelie.Application/Services/Persistence/ILeaseRepository.vb
@@ -3,7 +3,5 @@
 Namespace Services.Persistence
     Public Interface ILeaseRepository
         Inherits IRepository(Of Lease)
-        Function GetDetails As List(Of LeaseDetail)
-        Function GetDetailsAsync As Task(Of List(Of LeaseDetail))
     End Interface
 End NameSpace
