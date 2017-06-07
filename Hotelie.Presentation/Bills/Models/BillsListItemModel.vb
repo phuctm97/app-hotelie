ee37dc8a628875b721bc1172c11401f51ca36649
phuctm97 <phuctm97@gmail.com>
2017-06-08 01:32:57 +0700

    d

---
+++ Hotelie.Presentation/Bills/Models/BillsListItemModel.vb
@@ -0,0 +1,22 @@
+﻿Imports Caliburn.Micro
+
+Namespace Bills.Models
+	Public Class BillsListItemModel
+		Inherits PropertyChangedBase
+
+		Private _id As String
+
+		Public Property Id As String
+			Get
+				Return _id
+			End Get
+			Set
+				If IsNothing(Value) OrElse Equals(Value, _id) Then Return
+				_id = value
+				NotifyOfPropertyChange(Function() Id)
+			End Set
+		End Property
+
+		Public Property 
+	End Class
+End Namespace
