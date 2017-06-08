aab51334da9566d03f4f79f31d25942b63bc8617
phuctm97 <phuctm97@gmail.com>
2017-06-09 03:55:13 +0700

    filter

--- Hotelie.Presentation/Leases/Models/FilterLeaseModel.vb
+++ Hotelie.Presentation/Leases/Models/FilterLeaseModel.vb
@@ -34,10 +34,20 @@ Namespace Leases.Models
 				Return lease.TotalExpense >= expense
 			End If
 
-			Dim checkinDate As Date = Today
-			If Date.TryParse( text, checkinDate )
-				Return lease.CheckinDate > checkinDate
-			End If
+			For Each s As String In {"/", "\", "-"}
+				If text.Contains( s )
+					Dim x1, x2 As Integer
+					If Integer.TryParse( text.Split( s )( 0 ), x1 ) And
+					   Integer.TryParse( text.Split( s )( 1 ), x2 )
+						If x2 < 2000 Or x1 <= 0 Then Continue For
+
+						Try
+							Return lease.CheckinDate >= New Date( x2, x1, 1 )
+						Catch
+						End Try
+					End If
+				End If
+			Next
 
 			For Each detail As ILeaseDetailModel In lease.Details
 				If detail.CustomerName.ToLower().Contains( text ) Then Return True
