249d4584da6b706bcfa55a9511321f92b1b229a0
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-09 02:11:01 +0700

    reports

---
+++ Hotelie.Application/Reports/IGetRoomCategoriesProfit.vb
@@ -0,0 +1,6 @@
+﻿Namespace Reports
+    Public Interface IGetRoomCategoriesProfit
+        Function Execute(fromDays As Date, toDays As Date) As List(Of ProfitReportModel)
+        Function ExecuteAsync(month As Date, toDays As Date) As Task(Of List(Of ProfitReportModel))
+    End Interface
+End NameSpace
---
+++ Hotelie.Application/Reports/RoomUsedReportModel.vb
@@ -0,0 +1,6 @@
+﻿Namespace Reports
+    Public Class RoomUsedReportModel
+        Property Room As String
+        Property UsedDays As Integer
+    End Class
+End NameSpace
