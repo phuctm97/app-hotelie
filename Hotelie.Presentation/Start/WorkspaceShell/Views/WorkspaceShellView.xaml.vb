6dcca11f138fac522d9269f9116b1bee7469e8f8
phuctm97 <phuctm97@gmail.com>
2017-06-09 21:49:33 +0700

    before reload

--- Hotelie.Presentation/Start/WorkspaceShell/Views/WorkspaceShellView.xaml.vb
+++ Hotelie.Presentation/Start/WorkspaceShell/Views/WorkspaceShellView.xaml.vb
@@ -1,4 +1,11 @@
-﻿Namespace Start.WorkspaceShell.Views
+﻿Imports Hotelie.Presentation.Common.Controls
+
+Namespace Start.WorkspaceShell.Views
 	Public Class WorkspaceShellView
+		Implements INeedWindowModals
+
+		Private Sub OnShellLoaded( sender As Object,
+		                                 e As RoutedEventArgs )
+		End Sub
 	End Class
 End Namespace
