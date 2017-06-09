26042f175bb7f39b184cd493bb04368ba7829a12
Khuongnb1997 <khuongnb1997@gmail.com>
2017-06-09 23:24:24 +0700

    get can remove room category

---
+++ Hotelie.Application/Rooms/Queries/IGetCanRemoveRoomCategoryQuery.vb
@@ -0,0 +1,6 @@
+﻿Namespace Rooms.Queries
+    Public Interface IGetCanRemoveRoomCategoryQuery
+        Function Execute(id As String) As Boolean
+        Function ExecuteAsync(id As String) As Task(Of Boolean)
+    End Interface
+End NameSpace
